802bbf6b317aeaeda5919a622bd8e2b38354a14f
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-30 17:18:54 +0300

    commit2 mistakes fixed

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
      Scanner scanner=new Scanner(System.in);
-     double armut=2.14;
-     double elma=3.67;
-     double domates=1.11;
-     double muz=0.95;
-     double patlıcan=5;
-     int tane1,tane2,tane3,tane4,tane5;
-     System.out.println("Armut kaç kilo:");
-tane1= scanner.nextInt();
-        System.out.println("Elma kaç kilo:");
-        tane2= scanner.nextInt();
-        System.out.println("Domates kaç kilo:");
-        tane3= scanner.nextInt();
-        System.out.println("Muz kaç kilo:");
-        tane4= scanner.nextInt();
-        System.out.println("Patlıcan kaç kilo:");
-        tane5= scanner.nextInt();
-        double toplam=(armut*tane1)+(elma*tane2)+(domates*tane3)+(muz*tane4)+(patlıcan*tane5);
+     double pear=2.14;
+     double apple=3.67;
+     double tomato=1.11;
+     double banana=0.95;
+     double eggplant=5;
+     int kilo1,kilo2,kilo3,kilo4,kilo5;
+     System.out.println("How many kilos of pear:");
+kilo1= scanner.nextInt();
+        System.out.println("How many kilos of apple:");
+        kilo2= scanner.nextInt();
+        System.out.println("How many kilos of tomato:");
+        kilo3= scanner.nextInt();
+        System.out.println("How many kilos of banana:");
+        kilo4= scanner.nextInt();
+        System.out.println("How many kilos of eggplant:");
+        kilo5= scanner.nextInt();
+        double toplam=(pear*kilo1)+(apple*kilo2)+(tomato*kilo3)+(banana*kilo4)+(eggplant*kilo5);
         System.out.println("Toplam tutar:"+toplam+"TL");
     }
 }
